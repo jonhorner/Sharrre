033ca432c84d402e81731085b10b4e8c09b2947b
Jon Horner <jon.horner@gmail.com>
2012-11-19 09:23:24 +0000

    Added buttonsClass and buttonClass options to allow customising the classes used
    - defaults to the original 'buttons' and 'button' generic classes

--- jquery.sharrre.js
+++ jquery.sharrre.js
@@ -22,6 +22,8 @@
       linkedin    : false,
       pinterest   : false
     },
+    buttonsClass   : 'buttons',
+    buttonClass    : 'button',
     shareTotal     : 0,
     template       : '',
     title          : '',
@@ -119,7 +121,7 @@
     googlePlus : function(self){
       var sett = self.options.buttons.googlePlus;
       //$(self.element).find('.buttons').append('<div class="button googleplus"><g:plusone size="'+self.options.buttons.googlePlus.size+'" href="'+self.options.url+'"></g:plusone></div>');
-      $(self.element).find('.buttons').append('<div class="button googleplus"><div class="g-plusone" data-size="'+sett.size+'" data-href="'+(sett.url !== '' ? sett.url : self.options.url)+'" data-annotation="'+sett.annotation+'"></div></div>');
+      $(self.element).find('.'+self.options.buttonsClass).append('<div class="'+self.options.buttonClass+' googleplus"><div class="g-plusone" data-size="'+sett.size+'" data-href="'+(sett.url !== '' ? sett.url : self.options.url)+'" data-annotation="'+sett.annotation+'"></div></div>');
       window.___gcfg = {
         lang: self.options.buttons.googlePlus.lang
       };
@@ -138,7 +140,7 @@
     },
     facebook : function(self){
       var sett = self.options.buttons.facebook;
-      $(self.element).find('.buttons').append('<div class="button facebook"><div id="fb-root"></div><div class="fb-like" data-href="'+(sett.url !== '' ? sett.url : self.options.url)+'" data-send="'+sett.send+'" data-layout="'+sett.layout+'" data-width="'+sett.width+'" data-show-faces="'+sett.faces+'" data-action="'+sett.action+'" data-colorscheme="'+sett.colorscheme+'" data-font="'+sett.font+'" data-via="'+sett.via+'"></div></div>');
+      $(self.element).find('.'+self.options.buttonsClass).append('<div class="'+self.options.buttonClass+' facebook"><div id="fb-root"></div><div class="fb-like" data-href="'+(sett.url !== '' ? sett.url : self.options.url)+'" data-send="'+sett.send+'" data-layout="'+sett.layout+'" data-width="'+sett.width+'" data-show-faces="'+sett.faces+'" data-action="'+sett.action+'" data-colorscheme="'+sett.colorscheme+'" data-font="'+sett.font+'" data-via="'+sett.via+'"></div></div>');
       var loading = 0;
       if(typeof FB === 'undefined' && loading == 0){
         loading = 1;
@@ -156,7 +158,7 @@
     },
     twitter : function(self){
       var sett = self.options.buttons.twitter;
-      $(self.element).find('.buttons').append('<div class="button twitter"><a href="https://twitter.com/share" class="twitter-share-button" data-url="'+(sett.url !== '' ? sett.url : self.options.url)+'" data-count="'+sett.count+'" data-text="'+self.options.text+'" data-via="'+sett.via+'" data-hashtags="'+sett.hashtags+'" data-related="'+sett.related+'" data-lang="'+sett.lang+'">Tweet</a></div>');
+      $(self.element).find('.'+self.options.buttonsClass).append('<div class="'+self.options.buttonClass+' twitter"><a href="https://twitter.com/share" class="twitter-share-button" data-url="'+(sett.url !== '' ? sett.url : self.options.url)+'" data-count="'+sett.count+'" data-text="'+self.options.text+'" data-via="'+sett.via+'" data-hashtags="'+sett.hashtags+'" data-related="'+sett.related+'" data-lang="'+sett.lang+'">Tweet</a></div>');
       var loading = 0;
       if(typeof twttr === 'undefined' && loading == 0){
         loading = 1;
@@ -175,7 +177,7 @@
     },
     digg : function(self){
       var sett = self.options.buttons.digg;
-      $(self.element).find('.buttons').append('<div class="button digg"><a class="DiggThisButton '+sett.type+'" rel="nofollow external" href="http://digg.com/submit?url='+encodeURIComponent((sett.url !== '' ? sett.url : self.options.url))+'"></a></div>');
+      $(self.element).find('.'+self.options.buttonsClass).append('<div class="'+self.options.buttonClass+' digg"><a class="DiggThisButton '+sett.type+'" rel="nofollow external" href="http://digg.com/submit?url='+encodeURIComponent((sett.url !== '' ? sett.url : self.options.url))+'"></a></div>');
       var loading = 0;
       if(typeof __DBW === 'undefined' && loading == 0){
         loading = 1;
@@ -203,8 +205,8 @@
       if(typeof count === "undefined"){
         count = 0;
       }
-      $(self.element).find('.buttons').append(
-      '<div class="button delicious"><div style="'+css+'font:12px Arial,Helvetica,sans-serif;cursor:pointer;color:#666666;display:inline-block;float:none;height:20px;line-height:normal;margin:0;padding:0;text-indent:0;vertical-align:baseline;">'+
+      $(self.element).find('.'+self.options.buttonsClass).append(
+      '<div class="'+self.options.buttonClass+' delicious"><div style="'+css+'font:12px Arial,Helvetica,sans-serif;cursor:pointer;color:#666666;display:inline-block;float:none;height:20px;line-height:normal;margin:0;padding:0;text-indent:0;vertical-align:baseline;">'+
       '<div style="'+cssCount+'background-color:#fff;margin-bottom:5px;overflow:hidden;text-align:center;border:1px solid #ccc;border-radius:3px;">'+count+'</div>'+
       '<div style="'+cssShare+'display:block;padding:0;text-align:center;text-decoration:none;width:50px;background-color:#7EACEE;border:1px solid #40679C;border-radius:3px;color:#fff;">'+
       '<img src="http://www.delicious.com/static/img/delicious.small.gif" height="10" width="10" alt="Delicious" /> Add</div></div></div>');
@@ -215,7 +217,7 @@
     },
     stumbleupon : function(self){
       var sett = self.options.buttons.stumbleupon;
-      $(self.element).find('.buttons').append('<div class="button stumbleupon"><su:badge layout="'+sett.layout+'" location="'+(sett.url !== '' ? sett.url : self.options.url)+'"></su:badge></div>');
+      $(self.element).find('.'+self.options.buttonsClass).append('<div class="'+self.options.buttonClass+' stumbleupon"><su:badge layout="'+sett.layout+'" location="'+(sett.url !== '' ? sett.url : self.options.url)+'"></su:badge></div>');
       var loading = 0;
       if(typeof STMBLPN === 'undefined' && loading == 0){
         loading = 1;
@@ -237,7 +239,7 @@
     },
     linkedin : function(self){
       var sett = self.options.buttons.linkedin;
-      $(self.element).find('.buttons').append('<div class="button linkedin"><script type="in/share" data-url="'+(sett.url !== '' ? sett.url : self.options.url)+'" data-counter="'+sett.counter+'"></script></div>');
+      $(self.element).find('.'+self.options.buttonsClass).append('<div class="'+self.options.buttonClass+' linkedin"><script type="in/share" data-url="'+(sett.url !== '' ? sett.url : self.options.url)+'" data-counter="'+sett.counter+'"></script></div>');
       var loading = 0;
       if(typeof window.IN === 'undefined' && loading == 0){
         loading = 1;
@@ -253,7 +255,7 @@
     },
     pinterest : function(self){
       var sett = self.options.buttons.pinterest;
-      $(self.element).find('.buttons').append('<div class="button pinterest"><a href="http://pinterest.com/pin/create/button/?url='+(sett.url !== '' ? sett.url : self.options.url)+'&media='+sett.media+'&description='+sett.description+'" class="pin-it-button" count-layout="'+sett.layout+'">Pin It</a></div>');
+      $(self.element).find('.'+self.options.buttonsClass).append('<div class="'+self.options.buttonClass+' pinterest"><a href="http://pinterest.com/pin/create/button/?url='+(sett.url !== '' ? sett.url : self.options.url)+'&media='+sett.media+'&description='+sett.description+'" class="pin-it-button" count-layout="'+sett.layout+'">Pin It</a></div>');
 
       (function() {
         var li = document.createElement('script');li.type = 'text/javascript';li.async = true;
@@ -363,9 +365,9 @@
   Plugin.prototype.init = function () {
     var self = this;
     if(this.options.urlCurl !== ''){
-      urlJson.googlePlus = this.options.urlCurl + '?url={url}&type=googlePlus'; // PHP script for GooglePlus...
+      urlJson.googlePlus  = this.options.urlCurl + '?url={url}&type=googlePlus'; // PHP script for GooglePlus...
       urlJson.stumbleupon = this.options.urlCurl + '?url={url}&type=stumbleupon'; // PHP script for Stumbleupon...
-      urlJson.pinterest = this.options.urlCurl + '?url={url}&type=pinterest'; // PHP script for Pinterest...
+      urlJson.pinterest   = this.options.urlCurl + '?url={url}&type=pinterest'; // PHP script for Pinterest...
     }
     $(this.element).addClass(this.options.className); //add class
     
@@ -409,7 +411,7 @@
     //add hover event
     $(this.element).hover(function(){
       //load social button if enable and 1 time
-      if($(this).find('.buttons').length === 0 && self.options.enableHover === true){
+      if($(this).find('.'+self.options.buttonsClass).length === 0 && self.options.enableHover === true){
         self.loadButtons();
       }
       self.options.hover(self, self.options);
@@ -428,7 +430,7 @@
   ================================================== */
   Plugin.prototype.loadButtons = function () {
     var self = this;
-    $(this.element).append('<div class="buttons"></div>');
+    $(this.element).append('<div class="'+self.options.buttonsClass+'"></div>');
     $.each(self.options.share, function(name, val) {
       if(val == true){
         loadButton[name](self);
